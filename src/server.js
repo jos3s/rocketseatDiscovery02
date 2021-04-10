6ee061f4fba9a5ab5f2c1cb732326f68600b617e
Jos3s <karatedkgt17@gmail.com>
2021-04-10 19:12:46 -0300

    Permitindo que o express use o req.body

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ const routes=require("./routes");
 
 const server=express();
 
+server.use(express.urlencoded({extended:true}));
 server.set("view engine","ejs");
 server.use(express.static("public"));
 server.use(routes);
