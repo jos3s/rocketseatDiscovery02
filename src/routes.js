676842ab5f9514edb4b20b5489bd127059aef0d0
Jos3s <karatedkgt17@gmail.com>
2021-04-10 01:03:02 -0300

    Atualizando o routes com um objeto profile passado para a página profile

--- src/routes.js
+++ src/routes.js
@@ -3,9 +3,19 @@ const routes=express.Router();
 
 const views=__dirname+"/views/";
 
-routes.get("/",(req,res) => res.render(views+"index") );
-routes.get("/job",(req,res) => res.render(views+"job") );
-routes.get("/job/edit",(req,res) => res.render(views+"job-edit") );
-routes.get("/profile",(req,res) => res.render(views+"profile") );
+const profile={
+    name:"José Ulisses",
+    avatar:"https://avatars.githubusercontent.com/u/50359547?v=4",
+    "monthly-budget":3000,
+    "days-per-week":5,
+    "hours-per-day":5,
+    "vacation-per-year":10,
+}
+
+
+routes.get("/",(req,res) => res.render(views+"index"));
+routes.get("/job",(req,res) => res.render(views+"job"));
+routes.get("/job/edit",(req,res) => res.render(views+"job-edit"));
+routes.get("/profile",(req,res) => res.render(views+"profile",{profile}));
 
 module.exports=routes;
