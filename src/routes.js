da74038c90397f321e2c395e20127d8f271d748d
Jos3s <karatedkgt17@gmail.com>
2021-04-10 18:56:27 -0300

    Adicionando rota POST para /job e criando array de jobs

--- src/routes.js
+++ src/routes.js
@@ -12,9 +12,36 @@ const profile={
     "vacation-per-year":10,
 }
 
+const jobs=[
+    {
+        id:1,
+        name: "Pizzaria Guloso",
+        "daily-hours":2,
+        "total-hours":60,
+        createdAt=Date.now(),
+    },
+    {
+        id:2,
+        name: "OneTwo Project",
+        "daily-hours":4,
+        "total-hours":47,
+        createdAt=Date.now(),
+    }
+]
 
-routes.get("/",(req,res) => res.render(views+"index"));
+routes.get("/",(req,res) => res.render(views+"index", {jobs}));
 routes.get("/job",(req,res) => res.render(views+"job"));
+routes.post("/job",(req,res) =>{
+    const lastId=jobs[jobs.length-1]?.id || 1;;
+    jobs.push({
+        id:lastId+1,
+        name: req.body.name,
+        "daily-hours":req.body["daily-hours"],
+        "total-hours":req.body["total-hours"],
+        createdAt=Date.now(),
+    });
+    return res.redirect("/");
+});
 routes.get("/job/edit",(req,res) => res.render(views+"job-edit"));
 routes.get("/profile",(req,res) => res.render(views+"profile",{profile}));
 
