0ff50c871cfffa1ff6c3b976a95fc2d83310a5ee
Jos3s <karatedkgt17@gmail.com>
2021-04-11 15:20:21 -0300

    Refatorando o get e o create para usar o banco de dados

--- src/model/Job.js
+++ src/model/Job.js
@@ -1,31 +1,43 @@
-let data=[
-    {
-        id:1,
-        name: "Pizzaria Guloso",
-        "daily-hours":2,
-        "total-hours":1,
-        createdAt: Date.now()
-    },
-    {
-        id:2,
-        name: "OneTwo Project",
-        "daily-hours":4,
-        "total-hours":47,
-        createdAt:Date.now()
-    }
-]
+const Database=require("../db/config");
+
 
 module.exports={
-    get(){
-        return data;
-    },
-    update(newData){
-        data=newData;
-    },
-    delete(id){
-        data=data.filter(job=>Number(job.id)!==Number(id));
-    },
-    create(newJob){
-        data.push(newJob);
-    }
-}
+	async get(){
+		const db=await Database();
+
+		const jobs=await db.all(`SELECT * FROM jobs`);
+
+		await db.close();
+		return jobs.map((job)=>({
+			id:job.id,
+			name:job.name,
+
+			"daily-hours":job.daily_hours,
+			"total-hours":job.total_hours,
+			createdAt:job.created_at
+		}));
+	},
+	update(newData){
+		data=newData;
+	},
+	delete(id){
+		data=data.filter(job=>Number(job.id)!==Number(id));
+	},
+	async create(newJob){
+		const db=await Database();
+
+		await db.run(`INSERT INTO jobs (
+			name,
+			daily_hours,
+			total_hours,
+			created_at
+		) VALUES (
+			"${newJob.name}",
+			${newJob["daily-hours"]},
+			${newJob["total-hours"]},
+			${newJob.createdAt}
+		)`);
+
+		await db.close();
+	}
+};
