6e13911cbe7337b121991f95a649292f026f272d
Jos3s <karatedkgt17@gmail.com>
2021-04-11 14:22:49 -0300

    Refatorando as funções update e get para usar o banco de dados

--- src/model/Profile.js
+++ src/model/Profile.js
@@ -1,21 +1,39 @@
 const { update } = require("../controllers/ProfileController");
+const Database=require("../db/config");
 
-let data={
-    name:"José Ulisses",
-    avatar:"https://avatars.githubusercontent.com/u/50359547?v=4",
-    "monthly-budget":3000,
-    "days-per-week":5,
-    "hours-per-day":5,
-    "vacation-per-year":10,
-    "value-hour":75
-};
 
 module.exports={
-    get(){
-        return data;
+    async get(){
+        const db=await Database();
+        
+        const data=await db.get(`SELECT * FROM profile`);
+
+        await db.close();
+
+        return {
+            name: data.name,
+            avatar: data.avatar,
+            "monthly-budget": data.monthly_budget,
+            "days-per-week": data.days_per_week,
+            "hours-per-day": data.hours_per_day,
+            "vacation-per-year": data.vacation_per_year,
+            "value-hour": data.value_hour
+          };
     },
 
-    update(newData){
-        data=newData;
+    async update(newData){
+        const db=await Database();
+
+        await db.run(`UPDATE profile SET 
+            name= "${newData.name}",
+            avatar="${newData.avatar}",
+            monthly_budget=${newData["monthly-budget"]},
+            days_per_week=${newData["days-per-week"]},
+            hours_per_day=${newData["hours-per-day"]},
+            vacation_per_year=${newData["vacation-per-year"]},
+            value_hour=${newData["value-hour"]}
+        `);
+
+        await db.close();
     }
 };
