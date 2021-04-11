6c0b39b2fa1f549df7e8d026f887221d92796b44
Jos3s <karatedkgt17@gmail.com>
2021-04-11 01:16:35 -0300

    Modificando o modo servidor encontrar a pasta 'views'

--- src/server.js
+++ src/server.js
@@ -1,10 +1,13 @@
 const express=require("express");
 const routes=require("./routes");
+const path=require("path");
 
 const server=express();
 
-server.use(express.urlencoded({extended:true}));
 server.set("view engine","ejs");
+server.set("views",path.join(__dirname,"views"));
+
+server.use(express.urlencoded({extended:true}));
 server.use(express.static("public"));
 server.use(routes);
 
